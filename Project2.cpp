39e47d2c5aee4110349e5ea366f972253eacff00
sonka2598 <69012990+sonka2598@users.noreply.github.com>
2026-03-13 20:50:27 +0600

    Add files via upload

---
+++ Project2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int number{};
+	std::cout << "Введите число: \n";
+	std::cin >> number;
+	std::cout << "Вы ввели:\n" << number << std::endl;
+
+	std::string hello{};
+	std::cout << "Введите слово: \n";
+	std::cin >> hello;
+	std::cout << "Вы ввели:\n" << hello << std::endl;
+
+	return EXIT_SUCCESS;
+}
